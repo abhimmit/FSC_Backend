cf7f0cfb651159647e3b30932da474ccc01ac1b8
abhimmit <abhimmit@gmail.com>
2019-06-23 14:19:38 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require('express');
+// var cors = require('cors');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
+
+// set up express app
+const app = express();
+
+
+/*
+var allowedOrigins = ['//10.0.0.11:8081'];
+
+app.use(cors({
+
+  origin: function(origin, callback){
+    // allow requests with no origin
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+
+  exposedHeaders: ['Content-Length', 'X-Foo', 'X-Bar'],
+
+  credentials: true,
+}));
+*/
+
+//set up static files
+app.use(express.static('public'));
+
+// use body-parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use( (req, res, next) => {
+   res.header('Access-Control-Allow-Origin', '*');
+   res.header('Access-Control-Allow-Headers', 'Origin , X-Requested-With, Content-Type, Accept, Authorization');
+   if(req.method === 'OPTIONS') {
+	   res.header('Access-Control-Allow-Methods', 'PUT, PATCH, POST, DELETE , GET');
+	   return res.status(200).json({});
+   }
+   next();
+});
+
+
+// initialize routes
+app.use('/', require('./routes/api'));
+
+// Import configuration variables.
+require('dotenv').config({path: '.env'});
+
+// error handling middleware
+app.use(function(err, req, res, next){
+    console.log(err); // to see properties of message in our console
+    res.status(422).send({error: err.message});
+});
+
+// listen for requests
+let port = process.env.SERVER_PORT;
+var listener = app.listen( port || 4000 , function() {
+
+		console.log(`now listening for requests to ${ listener.address().port }`);
+	});
---
+++ routes/sendHotmail.js
@@ -0,0 +1,61 @@
+const nodemailer = require("nodemailer");
+
+// async..await is not allowed in global scope, must use a wrapper
+ async function sendHotmail(req , res){
+  // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+  // let testAccount = await nodemailer.createTestAccount();
+	
+	let data = req.body.data;
+	
+	process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+	
+	// Create the transporter with the required configuration for Outlook
+	// change the user and pass !
+	 var transporter = nodemailer.createTransport({
+		service: "Hotmail",
+		auth: {
+			user: 'abhimmit@hotmail.com',
+			pass: 'pater1959'
+		},
+		tls: {
+		   ciphers:'SSLv3',
+		   rejectUnauthorized: false
+		},
+		secureConnection: false, // TLS requires secureConnection to be false
+		
+		port: 587, // port for secure SMTP
+	});
+
+	// setup e-mail data, even with unicode symbols
+	var mailOptions = {
+		from: `Farida's Stress Counseling <abhimmit@hotmail.com>`, // sender address (who sends)
+		to: 'abhimmit@hotmail.com', // list of receivers (who receives)
+		subject: data.subject, // Subject line
+		text: data.description, // plaintext body
+		html: `<h3><b>Van : ${ data.name }</b></h3> Email adres : ${data.email} <br> Telefoon : ${data.telephone} <br> <p> ${ data.description } </p>`, // html body
+	};
+	// html: '<b>Hello world </b><br> This is the first email sent with Nodemailer in Node.js' // html body
+	
+	// send mail with defined transport object
+	let valid = true;
+	transporter.sendMail(mailOptions, function(error, info){
+		if(error){
+			return console.log(error);
+			valid = false;
+		};
+
+		// console.log('Message sent: ' + info.response);
+	}); 
+	
+	if(valid){
+        return res.status(200).send({
+            message: "Success"
+        });
+    }else{
+        return res.status(500).send({
+            message: "Failed"
+        });
+    }
+}
+module.exports = sendHotmail;
